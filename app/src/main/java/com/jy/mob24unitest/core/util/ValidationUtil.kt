f5e028f362d9197c0abd8972e1f3bcf4f5ddce31
Teoh Jie Yie <jieyiet2003@gmail.com>
2024-08-29 14:02:12 +0800

    commit circleci

--- app/src/main/java/com/jy/mob24unitest/core/util/ValidationUtil.kt
+++ app/src/main/java/com/jy/mob24unitest/core/util/ValidationUtil.kt
@@ -3,7 +3,7 @@ package com.jy.mob24unitest.core.util
 
 object ValidationUtil {
     fun validateEmail(email:String): Boolean {
-        val emailReg = Regex("[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+")
+        val emailReg = Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.(com)\$")
         return emailReg.matches(email)
     }
 }
